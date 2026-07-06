36f4186383a1c91b8d4ef1f487374633dbe1b7a5
Ahmadmahaboobshaik <ahmadmahaboobshaik@gmail.com>
2026-07-06 14:03:03 +0530

    Remove react-toastify

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import App from "./App";
 import "./index.css";
 
@@ -13,24 +10,9 @@ import { MedicineProvider } from "./context/MedicineContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-
       <MedicineProvider>
-
         <App />
-
-        <ToastContainer
-          position="top-right"
-          autoClose={2500}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          pauseOnHover
-          draggable
-          theme="colored"
-        />
-
       </MedicineProvider>
-
     </BrowserRouter>
   </React.StrictMode>
 );
